52ac3ac889514b7cfa4275243423bd5024a29522
Juan Novas <juanignacionovas@gmail.com>
2024-06-02 16:20:12 -0300

    bot creation

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+from  cogs.commands.pong import ping
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener el token del bot desde la variable de entorno
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
+# Configuración del bot
+prefix = "!"  # Puedes cambiar el prefijo del bot aquí si lo deseas
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+# Evento de inicio del bot
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.add_command(ping)
+
+# Iniciar el bot
+bot.run(TOKEN)
