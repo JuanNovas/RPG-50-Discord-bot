d30f8e073717bc194c1589aab46e5b6ca1f609d0
Juan Novas <juanignacionovas@gmail.com>
2024-06-02 19:51:22 -0300

    cog auto load

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import discord
+import asyncio
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-
-from  cogs.commands.pong import ping
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
@@ -20,12 +19,22 @@ intents.guilds = True
 prefix = "!"  # Puedes cambiar el prefijo del bot aquí si lo deseas
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
+# Cargar la extensión
+async def load_extensions():
+        await load("cogs/commands")
+
+async def load(folder):
+    for filename in os.listdir(folder):
+        if filename.endswith(".py") and filename != "__init__.py":
+            cog_name = folder.replace("/", ".").replace("\\",".") + "." + filename[:-3]
+            await bot.load_extension(cog_name)
+
 # Evento de inicio del bot
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-
-bot.add_command(ping)
+    await load_extensions()
 
 # Iniciar el bot
 bot.run(TOKEN)
+
