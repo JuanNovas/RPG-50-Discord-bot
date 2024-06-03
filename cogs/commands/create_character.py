55e86f363c7d5051955014c4416d6ce67b4b636a
juanimarcosdev <juanimarcos05@gmail.com>
2024-06-03 15:26:36 -0300

    user's reaction working and embed for bot answer

--- cogs/commands/create_character.py
+++ cogs/commands/create_character.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+import asyncio
+from discord import Embed
 
 class Create(commands.Cog):
     def __init__(self, bot):
@@ -7,26 +9,37 @@ class Create(commands.Cog):
 
     @commands.command(name="create")
     async def create(self, ctx):
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
         
         # dicord user's username
         username = ctx.message.author.name
 
-        classes = ['Warrior', 'Witcher', 'Paladin']
+        class_emojis = {
+            "⚔️": "Guerrero",
+            "🧙‍♂️": "Mago",
+            "🗡️": "Ladrón"
+        }
 
-        # asigns numbers for each class and waits for an answer
-        class_options = "\n".join([f"{i+1}. {cls}" for i, cls in enumerate(classes)])
-        await ctx.send(f"Select your class:\n{class_options}")
-        class_msg = await self.bot.wait_for('message', check=check)
-        class_choice = int(class_msg.content) - 1
+        # sends a message and waits for an answer via reactions.
+        class_message = await ctx.send("Select your class:")
+        for emoji in class_emojis.keys():
+            await class_message.add_reaction(emoji)
+        
+        # the reaction must be from the user that executed the command, must be an emoji defined in 'class_emojis'.
+        def reaction_check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) in class_emojis and reaction.message.id == class_message.id
+        
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', check=reaction_check, timeout=60.0)
+        except asyncio.TimeoutError:
+            await ctx.send("Time out.")
+            return
 
-        if 0 <= class_choice < len(classes):
-            clase = classes[class_choice]
-            await ctx.send(f"{username}, {clase}")
-        else:
-            await ctx.send("Invalid option, please try again")
-            await self.create(ctx)
+        clase = class_emojis[str(reaction.emoji)]
+        # Create an embed with the character information
+        embed = Embed(title=f"{user} - class", color=0x00ff00)  # Green color
+        embed.add_field(name="Class", value=clase)
+        await ctx.send(embed=embed)
+        
     
 async def setup(bot):
     await bot.add_cog(Create(bot))
