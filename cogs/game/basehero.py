ae540b60d6a8a81dc388e6a0fd7c2b3c4a4df447
Juan Novas <juanignacionovas@gmail.com>
2024-06-03 19:58:47 -0300

    crit chance and range added

--- cogs/game/basehero.py
+++ cogs/game/basehero.py
@@ -1,8 +1,14 @@
+import random
+
 class BaseHero():
         
     def do_attack(self, enemy, power=10):
-        damage =  ((self.attack / enemy.defense) / 10) * power
-        enemy.hp = damage
+        CRIT_CHANCE = 0.05
+        
+        damage =  (((self.attack / enemy.defense) / 10) * power) * random.uniform(0.8, 1)
+        if random.random() >= CRIT_CHANCE:
+            damage *= 2
+        enemy.hp -= damage
         return damage
     
     def is_alive(self):
