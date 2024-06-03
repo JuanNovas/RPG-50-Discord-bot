3b98a1d33b01c55c9012617a2aabd450df02432b
juanimarcosdev <juanimarcos05@gmail.com>
2024-06-03 20:38:22 -0300

    combat v0.0.1

--- cogs/game/basehero.py
+++ cogs/game/basehero.py
@@ -2,7 +2,7 @@ class BaseHero():
         
     def do_attack(self, enemy, power=10):
         damage =  ((self.attack / enemy.defense) / 10) * power
-        enemy.hp = damage
+        enemy.hp -= damage
         return damage
     
     def is_alive(self):
