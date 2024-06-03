c2b6b1a176b3ba2d5a17734d16b597dd16ab42a9
juanimarcosdev <juanimarcos05@gmail.com>
2024-06-03 18:24:54 -0300

    Buttons working fine and message deletion

--- cogs/commands/create_character.py
+++ cogs/commands/create_character.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
-import asyncio
 from discord import Embed
+from discord.ui import Button, View
+from discord import ButtonStyle  
 
 class Create(commands.Cog):
     def __init__(self, bot):
@@ -10,40 +11,40 @@ class Create(commands.Cog):
     @commands.command(name="create")
     async def create(self, ctx):
         
-        class_emojis = {
-            "⚔️": "Warrior",
-            "🧙‍♂️": "Magician",
-            "🗡️": "Thief"
+        # Each class with their picture 
+        class_images = {
+            "Warrior": "https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_640.jpg",
+            "🧙‍♂️ Magician": "https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_640.jpg",
+            "Thief": "https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_640.jpg"
         }
 
-        # sends a message and waits for an answer via reactions.
-        embed = Embed(title="CHOOSE YOUR CLASS!", color=0xADD8E6)
-        embed.add_field(name="WARRIOR", value="test")
-        embed.add_field(name="MAGICIAN", value="test")
-        embed.add_field(name="THIEF", value="test")
+        # sends a message and waits for an answer via buttons.
+        embed = Embed(title=f"{ctx.message.author.name}, CHOOSE YOUR CLASS!", color=0xADD8E6, description="Click a button to choose your class")
         embed.set_image(url="https://cdn.discordapp.com/attachments/474702643625984021/1247263771811119224/72fcadcd-905e-4db7-9065-ea523ca7638d.jpg?ex=665f6468&is=665e12e8&hm=5fc58203fe3fcffae54347b6726480f49e84e1ff4828bc0b16c22b7be4328013&")
-        class_message = await ctx.send(embed=embed)
-        for emoji in class_emojis.keys():
-            await class_message.add_reaction(emoji)
         
-        # the reaction must be from the user that executed the command, must be an emoji defined in 'class_emojis'.
-        def reaction_check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in class_emojis and reaction.message.id == class_message.id
+        #creates buttons 
+        view = View()
         
-        try:
-            reaction, user = await self.bot.wait_for('reaction_add', check=reaction_check, timeout=60.0)
-        except asyncio.TimeoutError:
-            await ctx.send("Time out.")
-            return
+        original_message = await ctx.send(embed=embed)
 
-        clase = class_emojis[str(reaction.emoji)]
-        # Create an embed with the character information
-        embed = Embed(title=f"{user} - class", description="Character created _succesfully_!", color=0xADD8E6)  # Green color
-        embed.add_field(name="class:", value=clase)
-        if clase == "Magician":
-            embed.set_image(url="https://cdn.discordapp.com/attachments/474702643625984021/1246905943997022299/310a8713-429a-41cf-835c-25e56a3873ee.jpg?ex=665ebfe7&is=665d6e67&hm=c17a5297bd9ffbd2aa06cc38a9613b167f3512fedb1432191e4109ab2201ca8f&")
-        await ctx.send(embed=embed)
-        
-    
+        for class_name, image_url in class_images.items():
+            button = Button(label=class_name, style=ButtonStyle.primary)
+            async def button_callback(interaction):
+                if interaction.user != ctx.author:
+                    return  # assure that the user is the same that started the command
+
+                embed = Embed(title=f"{interaction.user}'s class", description="Character created _successfully_!", color=0xADD8E6)
+                embed.add_field(name="Class:", value=class_name)
+                embed.set_image(url=image_url)
+                await interaction.response.send_message(embed=embed)
+                await original_message.delete()  # delete original message
+                button.callback = button_callback
+                view.add_item(button)
+
+            button.callback = button_callback
+            view.add_item(button)
+
+        await original_message.edit(view=view)
+
 async def setup(bot):
     await bot.add_cog(Create(bot))
