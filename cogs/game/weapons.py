aea2417467e234e4fdc5dd36383c4377f13bd51f
Juan Novas <juanignacionovas@gmail.com>
2024-06-04 12:06:17 -0300

    weapons attack

--- cogs/game/weapons.py
+++ cogs/game/weapons.py
@@ -1,4 +1,14 @@
 from cogs.game.baseweapon import BaseWeapon
 
 class WeaponKnife(BaseWeapon):
-    pass
+    def __init__(self):
+        super().__init__()
+        
+        self.plain["attack"] = 4
+        
+    def custom_attack(self, enemy):
+        damage = 10
+        enemy.hp -= damage
+        
+        message = f"{self.name} dealt {damage} damage to {enemy.name}"
+        return message
