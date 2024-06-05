c34955be34271963b46ac3878ef02e359c2c92eb
Juan Novas <juanignacionovas@gmail.com>
2024-06-04 22:03:47 -0300

    Hero escalability and dummys

--- cogs/game/characters.py
+++ cogs/game/characters.py
@@ -1,4 +1,5 @@
 from cogs.game.basehero import BaseHero
+from cogs.utils.decorators import mana_ability, health_ability
 
 class UserDummy(BaseHero):
     def __init__(self):
@@ -16,14 +17,50 @@ class UserDummy(BaseHero):
     def heal(self, *args):
         COST = 10
         HP_HEAL = 10
+        
         if self.mana >= COST:
             self.mana -= COST
             self.hp += HP_HEAL
             if self.hp > self.max_hp:
                 self.hp = self.max_hp
-                
-            message = f"{self.name} healed up to {self.hp} HP"
+        
+            message = f"healed up to {self.hp} HP"
         else:
             return False
         
         return message
+    
+    
+class MagicDummy(BaseHero):
+    def __init__(self):
+        super().__init__(
+            hp=20,
+            attack=3,
+            magic=7,
+            defense=3,
+            magic_resistance=4,
+            mana=20
+        )
+        
+    @mana_ability(cost=10)
+    def magic_flame(self, enemy):
+        return self.do_magic(self, enemy, 15)
+    
+class AssasinDummy(BaseHero):
+    def __init__(self):
+        super().__init__(
+            hp=30,
+            attack=5,
+            magic=2,
+            defense=4,
+            magic_resistance=5,
+            mana=10
+        )
+        
+    @mana_ability(cost=5)
+    def super_hit(self, enemy):
+        return self.do_attack(self, enemy, 20)
+    
+    @health_ability(cost=10)
+    def sacrifice(self, enemy):
+        return self.do_attack(self, enemy, 35)
--- cogs/utils/decorators.py
+++ cogs/utils/decorators.py
@@ -6,3 +6,21 @@ def same_user(original_func):
         result = original_func(*args, **kwargs)
         return result
     return decorator
+
+def mana_ability(cost):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            if not self.use_mana(cost):
+                return False
+            return func(self, *args, **kwargs)
+        return wrapper
+    return decorator
+
+def health_ability(cost):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            if not self.use_health(cost):
+                return False
+            return func(self, *args, **kwargs)
+        return wrapper
+    return decorator
