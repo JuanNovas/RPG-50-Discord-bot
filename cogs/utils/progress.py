62c501dc79debd7de4b2e615d9e3ad7023f444d1
Juan Novas <juanignacionovas@gmail.com>
2024-06-25 11:01:32 -0300

    Progress functions

---
+++ cogs/utils/progress.py
@@ -0,0 +1,31 @@
+from cogs.utils.database import execute
+
+
+def add_kill(user_id : int, amount=1):
+    execute('''
+    UPDATE advancements SET
+    kills = kills + (?)
+    WHERE hero_id = (
+        SELECT id FROM hero WHERE user_id = (?)
+    )
+    ''', (amount, user_id))
+    
+    
+def add_upgrade(user_id : int, amount=1):
+    execute('''
+    UPDATE advancements SET
+    upgrades = upgrades + (?)
+    WHERE hero_id = (
+        SELECT id FROM hero WHERE user_id = (?)
+    )
+    ''', (amount, user_id))
+    
+    
+def add_gold_spent(user_id : int, amount : int):
+    execute('''
+    UPDATE advancements SET
+    gold_spent = gold_spent + (?)
+    WHERE hero_id = (
+        SELECT id FROM hero WHERE user_id = (?)
+    )
+    ''', (amount, user_id))
