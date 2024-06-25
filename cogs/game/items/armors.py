b02ae465b4b32069d71bcdcf2c5b0e70964166de
Juan Novas <juanignacionovas@gmail.com>
2024-06-25 12:01:38 -0300

    FireScale armor added

--- cogs/game/items/armors.py
+++ cogs/game/items/armors.py
@@ -6,13 +6,26 @@ class ArmorIron(BaseArmor):
         super().__init__(**kwargs)
         
         self.plain["defense"] = 2
-        self.plain["magic resistance"] = 3
+        self.plain["magic_resistance"] = 3
         
         self.id = 1
         self.rarity = 1
         self.name = "iron"
+        
+        
+class ArmorScale(BaseArmor):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.plain["magic"] = 2
+        self.plain["magic_resistance"] = 5
+        
+        self.id = 2
+        self.rarity = 5
+        self.name = "scale"
 
 
 armor_dict =  {
-    1 : ArmorIron
+    1 : ArmorIron,
+    2 : ArmorScale
 }
