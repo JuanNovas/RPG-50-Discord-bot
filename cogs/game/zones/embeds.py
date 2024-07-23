7d23aedb5c7a9707cacc0f0e5da1d0063d5b8cc6
Juan Novas <juanignacionovas@gmail.com>
2024-07-23 10:48:02 -0300

    images added to zone selector

--- cogs/game/zones/embeds.py
+++ cogs/game/zones/embeds.py
@@ -8,31 +8,31 @@ zones_data = [
         "name": "🏕️ Train camp",
         "level": "Low",
         "loot": "Low",
-        "image": ""
+        "image": "https://cdn.discordapp.com/attachments/474702643625984021/1265302785864237106/DALLE_2024-07-23_10.41.24_-_A_medieval_training_field_with_various_training_dummies_archery_targets_and_wooden_training_weapons_scattered_around._The_field_is_surrounded_by_a_w.webp?ex=66a1048c&is=669fb30c&hm=b62a903d9dd1cdd051aa01478590a24b5a79cc470456dafbadfd041a261498f9&"
     },
     {
         "name": "⛓️ Dungeon",
         "level": "Low",
         "loot": "Low",
-        "image": ""
+        "image": "https://cdn.discordapp.com/attachments/474702643625984021/1265303722422960191/DALLE_2024-07-23_10.45.08_-_A_medieval_dungeon_interior_with_stone_walls_and_floors_dimly_lit_by_torches._The_scene_includes_iron_bars_a_few_wooden_barrels_and_a_wooden_table_.webp?ex=66a1056b&is=669fb3eb&hm=ca4e36de374748ba92ce8b553c2db2a08d19b046d0f12034ad4dbf95215dae4d&"
     },
     {
         "name": "🌲 Forest",
         "level": "Mid",
         "loot": "Low",
-        "image": ""
+        "image": "https://cdn.discordapp.com/attachments/474702643625984021/1265303908931207280/DALLE_2024-07-23_10.45.55_-_A_medieval_forest_scene_with_tall_dense_trees_and_thick_underbrush._Sunlight_filters_through_the_canopy_creating_dappled_shadows_on_the_forest_floor.webp?ex=66a10598&is=669fb418&hm=408df2146b1fbfae1386bd3c3ceb5fd568c9d286a215f1bff3e1926bf9d43a64&"
     },
     {
         "name": "🏜️ Desert",
         "level": "Mid",
         "loot": "Mid",
-        "image": ""
+        "image": "https://cdn.discordapp.com/attachments/474702643625984021/1265304075658989630/DALLE_2024-07-23_10.46.36_-_A_medieval_desert_scene_with_vast_sand_dunes_under_a_bright_clear_sky._Sparse_vegetation_like_dry_bushes_and_cacti_are_scattered_around._In_the_dista.webp?ex=66a105bf&is=669fb43f&hm=3e59d8437a6ca3eed933774322366cf53c57f72a029b599bca422f9b896f807b&"
     },
     {
         "name": "🐸 Swamp",
         "level": "High",
         "loot": "High",
-        "image": ""
+        "image": "https://cdn.discordapp.com/attachments/474702643625984021/1265304195309899907/DALLE_2024-07-23_10.47.04_-_A_medieval_swamp_scene_with_murky_water_dense_vegetation_and_twisted_gnarled_trees_draped_with_hanging_moss._The_swamp_is_shrouded_in_mist_creatin.webp?ex=66a105dc&is=669fb45c&hm=431a958de0a4e38fa982986d30ef88322a24252b215826eb8fbfd2ff0564a4e7&"
     }
 ]
 
