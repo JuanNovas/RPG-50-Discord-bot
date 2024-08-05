8677bdea13d5e449b49b40ac293346ebe6b8ef5d
Juan Novas <juanignacionovas@gmail.com>
2024-08-05 11:03:44 -0300

    schema update

--- data/schema.sql
+++ data/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS hero;
 DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS item_types;
 DROP TABLE IF EXISTS advancements;
+DROP TABLE IF EXISTS enemies;
+DROP TABLE IF EXISTS dex;
 
 CREATE TABLE hero (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -44,12 +46,45 @@ CREATE TABLE advancements (
     FOREIGN KEY (hero_id) REFERENCES hero(id)
 );
 
+CREATE TABLE enemies (
+    id INTEGER NOT NULL UNIQUE,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE dex (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    hero_id INTEGER NOT NULL,
+    enemy_id INTEGER NOT NULL,
+    FOREIGN KEY (hero_id) REFERENCES hero(id),
+    FOREIGN KEY (enemy_id) REFERENCES enemies(id)
+);
+
 
 CREATE INDEX user_id_index ON hero(user_id);
 
 -- Types INSERT
 INSERT INTO item_types (type) VALUES ('weapon');
 INSERT INTO item_types (type) VALUES ('armor');
+-- Enemies INSERT
+INSERT INTO enemies (id, name) VALUES (1, 'Dummy');
+INSERT INTO enemies (id, name) VALUES (2, 'Slime');
+INSERT INTO enemies (id, name) VALUES (3, 'LavaDragon');
+INSERT INTO enemies (id, name) VALUES (4, 'Skeleton');
+INSERT INTO enemies (id, name) VALUES (5, 'Goblin');
+INSERT INTO enemies (id, name) VALUES (6, 'Crow');
+INSERT INTO enemies (id, name) VALUES (7, 'Worm');
+INSERT INTO enemies (id, name) VALUES (8, 'Shadow');
+INSERT INTO enemies (id, name) VALUES (9, 'Rat');
+INSERT INTO enemies (id, name) VALUES (10, 'Wolf');
+INSERT INTO enemies (id, name) VALUES (11, 'GiantGoblin');
+INSERT INTO enemies (id, name) VALUES (12, 'Bear');
+INSERT INTO enemies (id, name) VALUES (13, 'Snake');
+INSERT INTO enemies (id, name) VALUES (14, 'Mummy');
+INSERT INTO enemies (id, name) VALUES (15, 'Scorpion');
+INSERT INTO enemies (id, name) VALUES (16, 'Golem');
+INSERT INTO enemies (id, name) VALUES (17, 'Scavengers');
+INSERT INTO enemies (id, name) VALUES (18, 'GiantFrog');
+
 
 -- Triggers
 CREATE TRIGGER create_advancements_entry AFTER INSERT ON hero
