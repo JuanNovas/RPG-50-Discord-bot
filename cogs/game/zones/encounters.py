4505b2a9f26c2c29d61180703db980304c4bde11
Juan Novas <juanignacionovas@gmail.com>
2024-08-13 11:18:39 -0300

    bonus level multipliyer added to encounters

--- cogs/game/zones/encounters.py
+++ cogs/game/zones/encounters.py
@@ -20,16 +20,20 @@ for route in routes:
     bosses.append(df[df['Boss'] == True].to_dict(orient='records'))
     
 
-def get_enemy_from_zone(zone_id: int):
+def get_enemy_from_zone(zone_id: int, bonus: float=None):
     enemy_data = random.choices(df_list[zone_id].to_dict(orient='records'), weights=probabilities[zone_id], k=1)[0]
     level = random.randint(enemy_data["Min-Level"], enemy_data["Max-Level"])
+    if bonus:
+        level = round(level * bonus)
     enemy = enemy_dict[enemy_data["ID"]](level=level)
     return enemy
 
 
-def get_boos_from_zone(zone_id: int):
+def get_boos_from_zone(zone_id: int, bonus: float=None):
     boss_data = random.choices(bosses[zone_id])[0]
     boss_level = random.randint(boss_data["Min-Level"], boss_data["Max-Level"])
+    if bonus:
+        boss_level = round(boss_level * bonus)
     boss = enemy_dict[boss_data["ID"]](level=boss_level)
     return boss
 
