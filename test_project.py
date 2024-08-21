10308991b9f52210a6934d55d748dde0b82b2458
Juan Novas <juanignacionovas@gmail.com>
2024-08-21 17:42:32 -0300

    test script added

---
+++ test_project.py
@@ -0,0 +1,17 @@
+import asyncio
+import pytest
+from project import *
+
+def test_get_token():
+    try:
+        get_token()
+    except RuntimeError as e:
+        pytest.fail(f"setup_bot raised an exception: {e}")
+        
+def test_load():
+    assert asyncio.run(load("cogs/test")) == True
+    with pytest.raises(FileNotFoundError):
+        asyncio.run(load("cogs/example"))
+
+def test_load_extensions():
+    assert asyncio.run(load_extensions()) == True
